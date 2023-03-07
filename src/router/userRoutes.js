5a9ef7171bf9985d619e021a6916df34209ee955
Moana.A <moana.amis@gmail.com>
2023-03-07 23:46:34 +0100

    quest part 2 jwt auth minor update

--- src/router/userRoutes.js
+++ src/router/userRoutes.js
@@ -7,13 +7,13 @@ const { sendToken, verifyToken } = require ("../middlewares/tokenHandler.js")
 
 const router = express.Router()
 
-router.post("/signup", HashPassword, postUser)
+router.post("/register", HashPassword, postUser)
 router.post("/login", getUserWithPasswordAndPassToNext, DeHashPassword, sendToken)
+router.get("/", getAllUsers);
+router.get("/:id", getUserById)
 // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX WALL WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
 
 router.use(verifyToken);
 
-router.get("/", getAllUsers);
-router.get("/:id", getUserById)
 router.put("/update", HashPassword, upDateUser)
 module.exports = router;
